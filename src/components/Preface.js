eb1c91ade1c47b161a0454e51742d749788cfaf4
Noble Millie <noble.millie@gmail.com>
2018-11-27 22:18:20 -0600

    component structure

---
+++ src/components/Preface.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Preface extends Component {
+    constructor() {
+      super();
+    }
+    render() {
+      return(
+        <div className="preface" >preface page</div>
+      )
+    }
+}
