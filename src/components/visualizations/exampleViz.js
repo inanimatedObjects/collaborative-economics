5ef88d0aaaf7afdcd6b0a375d867f269e3790cf3
Caleb Kress <calebkress@gmail.com>
2018-11-29 20:52:47 -0600

    refactored bar chart - added color scheme and corrected math

--- src/components/visualizations/exampleViz.js
+++ src/components/visualizations/exampleViz.js
@@ -4,10 +4,7 @@ import * as d3 from 'd3';
 const width = 600;
 const height = 300;
 const margin = {top: 20, right: 5, bottom: 20, left: 40};
-const red = '#eb6a5b';
-const green = '#b6e86f';
-const blue = '#52b6ca';
-const colors = [blue, green, red];
+const colors = ['#90AFC5', '#336B87', '#2A3132', '#763626'];
 
 class ExampleViz extends Component {
   state = {
@@ -33,9 +30,8 @@ class ExampleViz extends Component {
     const bars = data.map(d => {
       return {
         x: xScale(d.item) + margin.right + margin.left,
-        y: height - ((d.cost * height) / (data[3].cost + (data[3].cost / 10))) - margin.top,
-        height: (d.cost * height) / (data[3].cost + (data[3].cost / 10)),
-        fill: 'lavender'
+        y: height - ((d.cost * (height - (margin.bottom + margin.top))) / (data[3].cost + (data[3].cost / 10))) - margin.top,
+        height: (d.cost * (height - (margin.bottom + margin.top))) / (data[3].cost + (data[3].cost / 10)),
       }
     });
     console.log(bars)
@@ -59,7 +55,7 @@ class ExampleViz extends Component {
     return (
       <svg width={width} height={height}>
         {this.state.bars.map((d, i) =>
-          (<rect key={i} x={d.x} y={d.y} width='50' height={d.height} fill={d.fill} />))}
+          (<rect key={i} x={d.x} y={d.y} width='50' height={d.height} fill={colors[i]} />))}
         <g>
           <g ref='xAxis' transform={`translate(0, ${height - margin.bottom})`} />
           <g ref='yAxis' transform={`translate(${margin.left}, 0)`} />
